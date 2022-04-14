26fe43231a8f0a7764cdc962e0db3c3b58a37399
Kevin Blasco <blascokevin@outlook.com>
2022-04-14 15:39:14 -0400

    Added Service Layer
    Implemented Service Layer classes and interfaces

---
+++ SuperHeroSightings/src/main/java/com/sg/superherosightings/service/LocationServiceLayerImpl.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.superherosightings.service;
+
+/**
+ *
+ * @author Blasc
+ */
+public class LocationServiceLayerImpl implements LocationServiceLayer 
+{
+    
+}
